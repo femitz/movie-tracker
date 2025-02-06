9424ddc54c7e2208d73de20a60675c81bd728973
Felipe Schmitz <schmitzdz14@gmail.com>
2025-02-06 13:38:38 -0300

    Adicao de biblioteca para a lista para melhor desempenho

--- frontend/movie-tracker-frontend/src/app/components/MovieList/MovieList.tsx
+++ frontend/movie-tracker-frontend/src/app/components/MovieList/MovieList.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './MovieList.module.css';
+import { FixedSizeList as List } from 'react-window';
+import { ListChildComponentProps } from 'react-window';
+
 
 interface Movie {
   id: number;
@@ -16,13 +19,24 @@ const MovieList: React.FC<MovieListProps> = ({ movies }) => {
   return (
     <div className={styles['movie-list']}>
       <h2>Movie List</h2>
-      <ul>
-        {movies.map(movie => (
-          <li key={movie.id}>
-            <strong>{movie.title}</strong> | {movie.genre} | {movie.watched_date}
-          </li>
-        ))}
-      </ul>
+      <List
+        height={400} // Altura da lista
+        itemCount={movies.length} // Quantidade de itens
+        itemSize={50} // Altura de cada item
+        width="100%"
+      >
+        {/* @ts-ignore */}
+        {({ index, style }: ListChildComponentProps) => { // Tipagem correta
+          const movie = movies[index];
+          return (
+            <div style={style} key={movie.id}>
+              <li className={styles['movie-list li']}>
+                <strong>{movie.title}</strong> | {movie.genre} | {movie.watched_date}
+              </li>
+            </div>
+          );
+        }}
+      </List>
     </div>
   );
 };
