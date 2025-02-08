0a136ab55ac89cf05cc465dd310b4dd709e13caa
Felipe Schmitz <schmitzdz14@gmail.com>
2025-02-08 16:52:18 -0300

    mudanca da estilizacao da pagina

--- frontend/movie-tracker-frontend/src/app/page.tsx
+++ frontend/movie-tracker-frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import MovieList from "./components/MovieList/MovieList";
 import AddMovieForm from "./components/AddMovieForm/AddMovieForm";
 import styles from "./page.module.css";
+import { time } from "console";
 
 interface Movie {
   id: number;
@@ -67,10 +68,27 @@ export default function Home() {
     }
   };
 
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const circle = document.querySelector(`.${styles["cursor-circle"]}`) as HTMLElement;
+      if (circle) {
+        circle.style.left = `${e.clientX}px`;
+        circle.style.top = `${e.clientY}px`;
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []); 
+
   return (
     <div className={styles.page}>
+      <div className={styles["cursor-circle"]}></div>
       <main className={styles.main}>
-        <h1 className={styles.header}>Movie Tracker</h1>
+        <h1 className={styles.header}>🍿 Movie Tracker</h1>
         <AddMovieForm onAddMovie={handleAddMovie} />
         <MovieList movies={movies} />
       </main>
