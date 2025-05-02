7b54a31cff1e4a828cbb7fcb65b8c7e521875898
Felipe Schmitz <schmitzdz14@gmail.com>
2025-05-02 14:16:41 -0300

    feat: adição de botão de logout e ajustes no layout da página de filmes

--- movie-tracker-frontend/src/app/movies/page.tsx
+++ movie-tracker-frontend/src/app/movies/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { useRouter } from 'next/navigation';
 import MovieList from "../components/MovieList/MovieList";
 import AddMovieForm from "../components/AddMovieForm/AddMovieForm";
 import styles from "../page.module.css";
 import { useMovies } from "../hooks/useMovies";
+import { auth } from "../services/api";
 
 export default function MoviesPage() {
   const { movies, addMovie, removeById } = useMovies();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    auth.logout();
+    router.push('/auth');
+  };
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
@@ -29,7 +37,12 @@ export default function MoviesPage() {
     <div className={styles.page}>
       <div className={styles["cursor-circle"]}></div>
       <main className={styles.main}>
-        <h1 className={styles.header}>🍿 Movie Tracker</h1>
+        <div className={styles.headerContainer}>
+          <h1 className={styles.header}>🍿 Movie Tracker</h1>
+          <button onClick={handleLogout} className={styles.logoutButton}>
+            Sair
+          </button>
+        </div>
         <AddMovieForm onAddMovie={addMovie} />
         <MovieList movies={movies} removeById={removeById}/>
       </main>
